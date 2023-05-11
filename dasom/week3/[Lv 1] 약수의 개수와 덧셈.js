fcb34240347589fe78dff313eed1e32a472addd2
Sodasom <1219dasom@gmail.com>
2023-05-11 11:21:32 +0900

    ✨ [Lv 1] 약수의 개수와 덧셈

---
+++ dasom/week3/[Lv 1] 약수의 개수와 덧셈.js
@@ -0,0 +1,12 @@
+function solution(left, right) {
+  let result = 0;
+  for (let i = left; i <= right; ++i) {
+    // 약수 개수
+    let count = 0;
+    for (let j = 1; j <= i; ++j) {
+      if (i % j === 0) count++;
+    }
+    count % 2 === 0 ? (result += i) : (result -= i);
+  }
+  return result;
+}
